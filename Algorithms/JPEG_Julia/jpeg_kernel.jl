99ce42a3dd5aa621488661b4e11229aa9f77fd4b
hackguy25 <blaz.rojc.si@gmail.com>
2018-12-20 21:59:50 +0100

    Prva implementacija JPEG algoritma v Julii.

---
+++ Algorithms/JPEG_Julia/jpeg_kernel.jl
@@ -0,0 +1,74 @@
+function jpeg(imageIn, imageOut, q_matrix, width, height)
+    
+    PI = 3.141592f0
+    
+    l_id0 = threadIdx().x
+    l_id1 = threadIdx().y
+    
+    g_id0 = blockIdx().x
+    g_id1 = blockIdx().y
+    
+    t_y   = (g_id0 - 1) * blockDim().x + l_id0
+    t_x   = (g_id1 - 1) * blockDim().y + l_id1
+    
+    idct                = @cuStaticSharedMem(Float32, (8, 8))
+    dct                 = @cuStaticSharedMem(Float32, (8, 8))
+    local_image         = @cuStaticSharedMem(UInt8,   (8, 8))
+    quantization_matrix = @cuStaticSharedMem(UInt32,  (8, 8))
+    
+    local_image[l_id0, l_id1] = imageIn[(t_y - 1) * width + t_x]
+	quantization_matrix[l_id0, l_id1] = q_matrix[(l_id0 - 1) * 8 + l_id1]
+    
+    sync_threads() # WAIT FOR ALL THREADS TO WRITE IN LOCAL MEMORY
+    
+    sum = 0.0f0
+    for n = 1:8
+        
+        sum += local_image[l_id0, n] * CUDAnative.cos((PI / 8) * (n - 0.5f0) * (l_id1 - 1))
+    end
+    dct[l_id0, l_id1] = sum
+    
+    sync_threads() # WAIT FOR ALL THREADS TO CALCULATE 1D DCT
+    
+    sum = 0.0f0
+    for n = 1:8
+        
+        sum += dct[n, l_id1] * CUDAnative.cos((PI / 8) * (n - 0.5f0) * (l_id0 - 1))
+    end
+    dct[l_id0, l_id1] = sum
+    
+    sync_threads() # WAIT FOR ALL THREADS TO CALCULATE 2D DCT
+    
+    dct[l_id0, l_id1] = dct[l_id0, l_id1] / quantization_matrix[l_id0, l_id1]
+	dct[l_id0, l_id1] = round(dct[l_id0, l_id1])
+	dct[l_id0, l_id1] = dct[l_id0, l_id1] * quantization_matrix[l_id0, l_id1]
+    
+    sync_threads() # WAIT FOR ALL THREADS TO DIVIDE DCT WITH Q_MATRIX AND ROUND AND MULTIPLY
+    
+    sum = 0.125f0 * dct[1, l_id1]
+    for n = 2:8
+        
+        sum += 0.25f0 * dct[n, l_id1] * CUDAnative.cos((PI / 8) * (n - 1) * (l_id0 - 0.5f0))
+    end
+    idct[l_id0, l_id1] = sum
+
+	sync_threads() # WAIT FOR ALL THREADS TO CALCULATE 1D inverse-DCT
+    
+    sum = 0.125f0 * idct[l_id0, 1]
+    for n = 2:8
+        
+        sum += 0.25f0 * idct[l_id0, n] * CUDAnative.cos((PI / 8) * (n - 1) * (l_id1 - 0.5f0))
+    end
+    
+    sync_threads() # WAIT FOR ALL THREADS TO CALCULATE 2D inverse-DCT
+    
+    tmpOut = idct[l_id0, l_id1]
+    tmpOut = round(tmpOut)
+    tmpOut = clamp(tmpOut, 0, 255)
+    #tmpOut = CUDAnative.Int32(tmpOut)
+    imageOut[(t_y - 1) * width + t_x] = tmpOut
+    
+    # imageOut[(t_y - 1) * width + t_x] = UInt8(clamp(CUDAnative.round(idct[l_id0, l_id1]), 0, 255))
+    
+    return nothing
+end
---
+++ Algorithms/JPEG_Julia/jpeg_main.jl
@@ -0,0 +1,58 @@
+# __precompile__(true) # ko vse deluje
+
+using FileIO
+using Images
+using CuArrays, CUDAnative, CUDAdrv
+
+file_name = "image_4096.png"
+#file_name = "image_1024.png"
+#file_name = "image_2048.png"
+#file_name = "image_4096.png"
+
+image_in = Gray.(load("../JPEG_basic/" * file_name))
+
+height, width = size(image_in)
+println("IMAGE_RESOLUTION: ", size(image_in)[1], "x", size(image_in)[2])
+
+image_in = Array{UInt8, 1}(reshape(rawview(channelview(image_in))', :))
+
+quantization_matrix = UInt32[
+    16, 11, 10, 16, 24, 40, 51, 61,
+    12, 12, 14, 19, 26, 58, 60, 55,
+    14, 13, 16, 24, 40, 57, 69, 56,
+    14, 17, 22, 29, 51, 87, 80, 62,
+    18, 22, 37, 56, 68,109,103, 77,
+    24, 35, 55, 64, 81,104,113, 92,
+    49, 64, 78, 87,103,121,120,101,
+    72, 92, 95, 98,112,100,103, 99
+] # WARNING !! 1D ARRAY - NOT 2D
+
+println("GPU start...");
+
+t1 = time_ns()
+
+include("jpeg_kernel.jl") # contains jpeg(imgIn, imgOut, qMatrix)
+
+imagein_mem_obj  = CuArrays.CuArray(image_in)
+qmatrix_mem_obj  = CuArrays.CuArray(quantization_matrix)
+imageout_mem_obj = Float32.(similar(imagein_mem_obj))
+
+WORKGROUP_SIZE = 8
+
+threadNum = (        WORKGROUP_SIZE,          WORKGROUP_SIZE)
+groupNum  = (width ÷ WORKGROUP_SIZE, height ÷ WORKGROUP_SIZE)
+
+@cuda jpeg(imagein_mem_obj, imageout_mem_obj, qmatrix_mem_obj, width, height)
+
+image_out = Array(imageout_mem_obj)
+image_out = UInt8.(image_out)
+
+t2 = time_ns()
+
+time2 = (t2 - t1) / 1000000
+println("GPU time: ", Int(round(time2)), " ms");
+
+image_out = colorview(Gray, reinterpret(N0f8, reshape(image_out', width, height)))
+save("image_compressed.png", image_out)
+
+nothing
